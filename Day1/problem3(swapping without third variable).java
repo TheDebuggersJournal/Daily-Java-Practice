29b3f6600e2589afaac745031552229feabad84c
TheDebuggersJournal <pratik45walunj@gmail.com>
2025-01-31 21:55:44 +0530

    Create problem3(swapping without third variable).java

---
+++ Day1/problem3(swapping without third variable).java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Swappingtwonumbers {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int number1 = sc.nextInt();
+        System.out.println("Enter number 2: ");
+        int number2 = sc.nextInt();
+        int number3 = number1;
+        number1 = number1 + number2;
+        number2 = number1 - number2;
+        number1 = number1 - number2;
+
+
+        System.out.printf("number1 after swapping is %d and number2 after swapping is %d",number1,number2);
+	}
+}
